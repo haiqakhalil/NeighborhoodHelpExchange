95fa2513f25c85a9e77bee2d13d27bdcfa620411
haiqakhalil28 <haiqakhalil28@gmail.com>
2026-06-30 15:00:21 +0500

    Added Post.java

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,4 +1,52 @@
 package model;
 
 public class Post {
+    private String postId;
+    private String userId;
+    private String type;
+    private String description;
+    private String status;
+
+    public Post(String description, String postId, String status, String type, String userId) {
+        this.description = description;
+        this.postId = postId;
+        this.status = status;
+        this.type = type;
+        this.userId = userId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "description='" + description + '\'' +
+                ", postId='" + postId + '\'' +
+                ", userId='" + userId + '\'' +
+                ", type='" + type + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
