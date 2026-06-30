595dee166168da632c5216d44a4d3819388328b3
haiqakhalil28@gmail.com <haiqakhalil28@email.com>
2026-06-30 14:46:27 +0500

    User.java added

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,45 @@
 package model;
 
 public class User {
+    private String userId;
+    private String username;
+    private String password;
+    private String area;
+
+    public User(String area, String password, String userId, String username) {
+        this.area = area;
+        this.password = password;
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "area='" + area + '\'' +
+                ", userId='" + userId + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
