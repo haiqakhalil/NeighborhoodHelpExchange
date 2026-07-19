fe3b9f2e853238e531ec7573f572721f614caa86
haiqakhalil28@gmail.com <haiqakhalil28@gmail.com>
2026-07-20 04:56:57 +0500

    Added Match model

--- src/models/Match.java
+++ src/models/Match.java
@@ -1,4 +1,25 @@
 package models;
 
 public class Match {
+    private String matchId;
+    private String postId;
+    private String requesterId;
+    private String helperId;
+
+    public Match(String matchId, String postId, String requesterId, String helperId) {
+        this.matchId = matchId;
+        this.postId = postId;
+        this.requesterId = requesterId;
+        this.helperId = helperId;
+    }
+
+    public String getMatchId() { return matchId; }
+    public String getPostId() { return postId; }
+    public String getRequesterId() { return requesterId; }
+    public String getHelperId() { return helperId; }
+
+    @Override
+    public String toString() {
+        return matchId + "," + postId + "," + requesterId + "," + helperId;
+    }
 }
