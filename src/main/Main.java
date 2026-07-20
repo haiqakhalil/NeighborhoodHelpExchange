8780a2567df8f6246be725d32205c60e5cbab480
haiqakhalil28@gmail.com <haiqakhalil28@gmail.com>
2026-07-20 05:05:08 +0500

    Added exception handling in Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,10 @@ package main;
 import gui.LoginScreen;
 import storage.FileHandler;
 import javax.swing.*;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FileHandler.createFilesIfNotExist();
         SwingUtilities.invokeLater(LoginScreen::new);
     }
